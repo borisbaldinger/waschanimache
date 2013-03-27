7a2fae37445dbef67f3a3521ca60cec1e12be85a
Boris Baldinger <boris@amazeelabs.com>
2013-03-27 18:03:47 +0100

    Removed koken

--- koken/storage/configuration/user_setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	// Path to ImageMagick on your server
-	define('MAGICK_PATH', 'convert');
-	
-	// Path to ffmpeg on your server
-	define('FFMPEG_PATH', 'ffmpeg');
--- koken/storage/themes/chastain/js/global.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$(window).on('resize',resizeView);
-	function resizeView() {
-		$('#container').css({
-			top: ( $('header').css('position') === 'fixed' ) ? $('header').height() : '0'
-		});
-	}
-	resizeView();
-});
